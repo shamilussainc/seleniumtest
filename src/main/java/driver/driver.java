6fec2336536d2687ea936b6c7145226d7e6229a7
shamilussainc <shamilhussainc@gmail.com>
2021-04-22 16:54:37 +0530

    Sampleproj

---
+++ src/main/java/driver/driver.java
@@ -0,0 +1,32 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class driver {
+	public WebDriver driver;
+	
+	public void fox(String url){
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.navigate().to(url);
+		driver.manage().window().maximize();
+	}
+	
+	public void chrome(String url) {
+		WebDriverManager.chromedriver().setup();				
+		ChromeOptions options = new ChromeOptions(); 
+		options.addArguments("disable-infobars");
+		options.addArguments("--disable-extensions");
+		DesiredCapabilities dc = new DesiredCapabilities();
+	    dc.setCapability(ChromeOptions.CAPABILITY, options);
+		driver = new ChromeDriver(options); 	
+		driver.navigate().to(url);
+		driver.manage().window().maximize();
+	}
+}
---
+++ src/test/java/test/samp/test.java
@@ -0,0 +1,19 @@
+package test.samp;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import driver.driver;
+
+public class test {
+	
+	driver dr = new driver();
+	
+	@Test(priority = 1)
+	public void main() {
+		dr.chrome("https://www.facebook.com/");
+		Assert.assertTrue(dr.driver.getTitle().contains("Facebook"));
+		dr.driver.close();
+		
+	}
+}
